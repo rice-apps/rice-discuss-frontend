e6ca7a065f349dde6dbdc25671c3e8ebbc579e53
Victor Song <vsong1618@gmail.com>
2019-10-26 12:55:13 -0500

    Initialize Grommet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Grommet from 'grommet';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Grommet plain>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </Grommet>
   );
 }
 
