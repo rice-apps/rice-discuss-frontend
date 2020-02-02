a997ad28d5982a92e489c7ad2c7473f7ea5fb812
Matthew Perez <matthewperez@Matthews-MacBook-Pro.local>
2020-02-02 14:58:21 -0600

    linked App.js to landing page / sign up page js files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import './components/button.css';
-import landingPage from './components/assets/login.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LandingPage from './landingPage.js';
+import SignUp from "./signUp.js"
+
 function App() {
   return (
-    <div style = {{backgroundImage: `url(${landingPage})`,backgroundSize: `contain`, height: `100%`, backgroundPosition:`center`, backgroundRepeat: `no-repeat`}}>
-    	<button class="button">Sign In</button>
+    <div>
+    	<SignUp/>
     </div>
   );
 }
