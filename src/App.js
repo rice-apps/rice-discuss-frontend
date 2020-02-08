c8d9577c3c398bd2786ef0b99f67fe3c0faf0b04
Matthew Perez <matthewperez@Matthews-MacBook-Pro.local>
2020-02-08 13:51:54 -0600

    landing page improvements ?

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import './components/button.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LandingPage from './landingPage.js';
-import SignUp from "./signUp.js"
+import SignUp from "./signUp.js";
 
 function App() {
   return (
     <div>
-    	<SignUp/>
+    	<LandingPage/>
     </div>
   );
 }
